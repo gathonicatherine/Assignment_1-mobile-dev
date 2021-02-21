50089c88d19b9ab4a4db7993b5599a154570cb41
gathonicatherine <catherrinewanjiku@gmail.com>
2021-02-21 11:45:27 +0300

    Asiignment_1 mobile dev git remote add origin https://github.com/gathonicatherine/Assignment_1-mobile-dev.git git push -u origin main second commit

---
+++ src/cate.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val name=("catherineWanjiku")
+    print ("name")
+
+    var age=20
+    age=21
+    print (age)
+
+    var phoneNumber:String=("0707986117")
+    println("phoneNumber")
+
+    var weightinkg:Number=60
+    println(weightinkg)
+
+    var citizen:Boolean=true
+    println(citizen)
+
+
+}
